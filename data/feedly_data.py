2470884807d693fd1f0acd39948b9a536e2c78ff
Peter Martigny <peter@feedly.com>
2019-04-04 18:18:29 -0700

    add basic like-board feedly data extractor

--- data/feedly_data.py
+++ data/feedly_data.py
@@ -49,7 +49,7 @@ class FeedlyDownloader:
         self.token = token
         self.df: pd.DataFrame = None
 
-    def get_entries(self, category: str, max_count: int, account: str='enterprise') -> List[Entry]:
+    def get_category_entries(self, category: str, max_count: int, account: str='enterprise') -> List[Entry]:
 
         with FeedlySession(auth=self.token) as sess:
             if account == 'enterprise':
@@ -75,9 +75,37 @@ class FeedlyDownloader:
 
         return entries
 
+
+    def get_board_entries(self, board: str, max_count: int, account: str='enterprise') -> List[Entry]:
+
+        with FeedlySession(auth=self.token) as sess:
+            if account == 'enterprise':
+                feeds = sess.user.get_enterprise_tags()
+            elif account == 'personal':
+                feeds = sess.user.get_tags()
+            else:
+                raise ValueError("Only enterprise and personal account are taken into account")
+            keep = None
+            for feed in feeds:
+                if feeds[feed].json['label'] == board:
+                    keep = feeds[feed]
+            print(keep)
+            category_id = keep.stream_id.content_id
+            entries = []
+            if account == 'enterprise':
+                entries = sess.user.get_enterprise_tag(category_id).stream_contents(options=StreamOptions(max_count=max_count))
+            elif account == 'personal':
+                entries = sess.user.get_tag(category_id).stream_contents(options=StreamOptions(max_count=max_count))
+            else:
+                raise ValueError("Only enterprise and personal account are taken into account")
+            entries = list(entries)
+
+        return entries
+
+
     def build_dataset(self, category: str, max_count: int, save_path: Path):
 
-        entries = self.get_entries(category=category, max_count=max_count)
+        entries = self.get_category_entries(category=category, max_count=max_count)
         shuffle(entries)
         self.df = build_dataframe(entries)
         limits = [int(0.8*len(entries)), int(0.1*len(entries))]
@@ -90,7 +118,7 @@ class FeedlyDownloader:
 
     def build_multi_class_dataset(self, categories: List[str], max_count: int, save_path: Path):
 
-        entries = {category: self.get_entries(category=category, max_count=max_count, account='personal') for category in categories}
+        entries = {category: self.get_category_entries(category=category, max_count=max_count, account='personal') for category in categories}
         [shuffle(entries[category]) for category in entries]
         entries = {category: build_dataframe(entries[category]) for category in entries}
         for category in entries:
@@ -104,26 +132,55 @@ class FeedlyDownloader:
         self.df = df
         self.df.to_csv(path_or_buf=save_path)
 
+    def build_like_board_dataset(self, category: str, boards: List[str], max_count: int, save_path: Path):
 
+        category_entries = self.get_category_entries(category=category, max_count=max_count)
+        shuffle(category_entries)
+        df_category = build_dataframe(entries=category_entries)
+        df_category['target'] = ['category'] * len(category_entries)
+        boards_entries = []
+        for board in boards:
+            boards_entries.extend(self.get_board_entries(board=board, max_count=max_count))
+        shuffle(boards_entries)
+        df_boards = build_dataframe(entries=boards_entries)
+        df_boards['target'] = ['board'] * len(df_boards)
+
+        for df in [df_category, df_boards]:
+            limits = [int(0.8 * len(df)), int(0.1 * len(df))]
+            split = ['train'] * limits[0] + ['val'] * limits[1]
+            split.extend(['test'] * (len(df) - len(split)))
+            df['split'] = split
+
+        df = pd.concat([df_category, df_boards])
+        df.to_csv(path_or_buf=save_path)
 
 
 if __name__ == "__main__":
 
-    token = "YourToken"
+    token ="YourToken"
     downloader = FeedlyDownloader(token=token)
-    save_path = Path.home() / 'work/experiments/nlp/data'
-    category = 'YourFeed'
-    max_count = 10000
-    save_path = save_path / 'feedly_data10000.csv'
-    downloader.build_dataset(category=category, max_count=max_count, save_path=save_path)
 
-    #Multilingual
-    token = "YourToken"
-    downloader = FeedlyDownloader(token=token)
-    categories = ["Category1", "Category2"]
-    max_count = 1000
-    save_path = Path.home() / 'work/experiments/nlp/data/feedly_multilingual.csv'
-    downloader.build_multi_class_dataset(categories=categories, max_count=max_count, save_path=save_path)
+    # # One category
+    # save_path = Path.home() / 'work/experiments/nlp/data'
+    # category = 'YourFeed'
+    # max_count = 10000
+    # save_path = save_path / 'feedly_data10000.csv'
+    # downloader.build_dataset(category=category, max_count=max_count, save_path=save_path)
+    #
+    # #Multilingual
+    # token = "YourToken"
+    # downloader = FeedlyDownloader(token=token)
+    # categories = ["Category1", "Category2"]
+    # max_count = 1000
+    # save_path = Path.home() / 'work/experiments/nlp/data/feedly_multilingual.csv'
+    # downloader.build_multi_class_dataset(categories=categories, max_count=max_count, save_path=save_path)
+
+    # # One category, One board
+    # category = "YourCategory"
+    # boards = ['YourFirstBoard', 'YourSecondBoard']
+    # max_count = 100
+    # save_path = save_path / 'feedly_data_lb.csv'
+    # downloader.build_dataset(category=category, boards=boards, max_count=max_count, save_path=save_path)
 
 
 
