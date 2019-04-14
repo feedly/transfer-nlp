759f9aeb9601a4bdca45c4c6e5eea4f785c51e2f
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-13 18:33:21 -0700

    Fixed PYTHON_E251 issues in data/download.py

--- data/download.py
+++ data/download.py
@@ -33,12 +33,12 @@ def download_file_from_google_drive(id, destination):
 
     session = requests.Session()
 
-    response = session.get(URL, params = { 'id' : id }, stream = True)
+    response = session.get(URL, params={ 'id' : id }, stream=True)
     token = get_confirm_token(response)
 
     if token:
         params = { 'id' : id, 'confirm' : token }
-        response = session.get(URL, params = params, stream = True)
+        response = session.get(URL, params=params, stream=True)
 
     save_response_content(response, destination)
 
