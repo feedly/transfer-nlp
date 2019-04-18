456951cf6dc6c381760b4808c2da2ce1d042e922
Peter Martigny <peter.martigny@gmail.com>
2019-04-17 17:01:44 -0700

    remove non-ignite events handler

--- transfer_nlp/runners/single_task.py
+++ transfer_nlp/runners/single_task.py
@@ -95,52 +95,6 @@ class Runner(RunnerABC):
         # Terminate if NaNs are created after an iteration
         self.trainer.add_event_handler(Events.ITERATION_COMPLETED, TerminateOnNan())
 
-    def update(self, batch_dict: Dict, running_loss: float, batch_index: int, running_metrics: Dict, compute_gradient: bool = True):
-        """
-        If compute_gradient is True, this is a training update, otherwise this is a validation / test pass
-        :param batch_dict:
-        :param running_loss:
-        :param batch_index:
-        :param running_metrics:
-        :param compute_gradient:
-        :return:
-        """
-        if compute_gradient:
-            self.optimizer.zero_grad()
-
-        model_inputs = {inp: batch_dict[inp] for inp in self.model.inputs_names}
-        y_pred = self.model(**model_inputs)
-
-        loss_params = {
-            "input": y_pred,
-            "target": batch_dict['y_target']}
-
-        if hasattr(self.loss.loss, 'mask') and self.mask_index:
-            loss_params['mask_index'] = self.mask_index
-
-        loss = self.loss.loss(**loss_params)
-        penalty = torch.Tensor([0])
-        if hasattr(self, "regularizer"):
-            penalty += self.regularizer.regularizer.compute_penalty(model=self.model)
-
-        loss_batch = loss.item() + penalty.item()
-        # TODO: see if we can improve the online average (check exponential average)
-        running_loss += (loss_batch - running_loss) / (batch_index + 1)
-
-        if compute_gradient:
-            loss.backward()  # TODO: See if we want to optimize loss or loss + penalty
-            # Gradient clipping
-            if hasattr(self, 'gradient_clipping'):
-                torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.gradient_clipping)
-
-            self.optimizer.step()
-
-        for metric in self.metrics.names:
-            metric_batch = self.metrics.metrics[metric](**loss_params)
-            running_metrics[f"running_{metric}"] += (metric_batch - running_metrics[f"running_{metric}"]) / (batch_index + 1)
-
-        return running_loss, running_metrics
-
 
 slack_webhook_url = "YourWebhookURL"
 slack_channel = "YourFavoriteSlackChannel"
@@ -159,7 +113,7 @@ if __name__ == "__main__":
     parser.add_argument('--config', type=str)
     args = parser.parse_args()
 
-    args.config = args.config or 'experiments/mlp.json'
+    args.config = args.config or 'experiments/newsClassifier.json'
     runner = Runner.load_from_project(experiment_file=args.config)
 
     if slack_webhook_url and slack_webhook_url != "YourWebhookURL":
