f2530e925a46770e3ca551639640b5cccde53637
Peter Martigny <peter.martigny@gmail.com>
2019-04-25 16:17:00 -0700

    only fine -tune classifier part

--- experiments/bert/runner.py
+++ experiments/bert/runner.py
@@ -13,15 +13,17 @@ if __name__ == "__main__":
     path = './bert.json'
     experiment = ExperimentConfig.from_json(path, HOME=home_env)
     # Prepare optimizer
+
+    layers = ['classifier.weight', 'classifier.bias']
+
+    [parameter.requires_grad_(False) for name, parameter in experiment['trainer'].model.named_parameters() if name not in layers]
     param_optimizer = list(experiment['trainer'].model.named_parameters())
-    no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
         {
-            'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
-            'weight_decay': 0.01},
-        {
-            'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
-            'weight_decay': 0.0}
+            'params': [p for n, p in param_optimizer if n in layers],
+            'weight_decay': 0.01}
     ]
+
     experiment['trainer'].optimizer = BertAdam(optimizer_grouped_parameters, lr=experiment['lr'])
     experiment['trainer'].train()
+
