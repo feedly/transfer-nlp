68175e036327692693ab3f326ee72e2d0644392d
Peter Martigny <peter.martigny@gmail.com>
2019-04-30 12:22:05 -0700

    fix path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 import sphinx_rtd_theme
 
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
