da35303535dac9c41e718658e3e6de190ded6b4d
Peter Martigny <peter.martigny@gmail.com>
2019-04-30 13:05:38 -0700

    mock ignite in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,20 @@
 #
 import os
 import sys
+from unittest.mock import MagicMock
 import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['ignite']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
 
