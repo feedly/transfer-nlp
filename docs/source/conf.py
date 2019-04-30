017e807042e27b27e29376df566ba5d4ad0e9ffa
Peter Martigny <peter.martigny@gmail.com>
2019-04-30 13:08:24 -0700

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['ignite']
+MOCK_MODULES = ['ignite', 'ignite.metrics']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
