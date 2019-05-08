8dc3528e029aaf15030bc13bb190a281a1c534fd
Peter Martigny <peter.martigny@gmail.com>
2019-05-08 14:51:24 -0700

    minor

--- experiments/training.py
+++ experiments/training.py
@@ -18,39 +18,35 @@ if __name__ == "__main__":
     cbow_path = './cbow.json'
     news_path = './newsClassifier.json'
 
-    # for path in surname_paths:
-    #     logger.info(f"Launching test for experiment {path}")
-    #     experiment = ExperimentConfig(path, HOME=home_env)
-    #     experiment['trainer'].train()
-    #     if 'predictor' in experiment:
-    #         input_json = {
-    #             "inputs": ["Zhang",
-    #                        "Mueller", 'Mahmoud', "Rastapopoulos"]}
-    #         output_json = experiment['predictor'].json_to_json(input_json=input_json)
-    #         logger.info(input_json)
-    #         logger.info(output_json)
-    #
-    # logger.info(f"Launching test for experiment {cbow_path}")
-    # path = cbow_path
-    # experiment = ExperimentConfig(path, HOME=home_env)
-    # experiment['trainer'].train()
-    # input_json = {
-    #     "inputs": ["I go to and take notes"]}
-    # output_json = experiment['predictor'].json_to_json(input_json=input_json)
-    # logger.info(input_json)
-    # logger.info(output_json)
-    #
-    # logger.info(f"Launching test for experiment {news_path}")
-    # path = news_path
-    # experiment = ExperimentConfig(path, HOME=home_env)
-    # experiment['trainer'].train()
-    # input_json = {
-    #     "inputs": ["Banking financing Asset Manager Gets OK To Appeal €15M Fee Payout Ruling",
-    #                "NASA's New Planet-Hunting Telescope Just Found Its First Earth-Sized World"]}
-    # output_json = experiment['predictor'].json_to_json(input_json=input_json)
-    # logger.info(input_json)
-    # logger.info(output_json)
+    for path in surname_paths:
+        logger.info(f"Launching test for experiment {path}")
+        experiment = ExperimentConfig2(path, HOME=home_env)
+        experiment['trainer'].train()
+        if 'predictor' in experiment:
+            input_json = {
+                "inputs": ["Zhang",
+                           "Mueller", 'Mahmoud', "Rastapopoulos"]}
+            output_json = experiment['predictor'].json_to_json(input_json=input_json)
+            logger.info(input_json)
+            logger.info(output_json)
 
-    path = './newsClassifier.json'
+    logger.info(f"Launching test for experiment {cbow_path}")
+    path = cbow_path
     experiment = ExperimentConfig2(path, HOME=home_env)
-    experiment.experiment['trainer'].train()
+    experiment['trainer'].train()
+    input_json = {
+        "inputs": ["I go to and take notes"]}
+    output_json = experiment['predictor'].json_to_json(input_json=input_json)
+    logger.info(input_json)
+    logger.info(output_json)
+
+    logger.info(f"Launching test for experiment {news_path}")
+    path = news_path
+    experiment = ExperimentConfig2(path, HOME=home_env)
+    experiment['trainer'].train()
+    input_json = {
+        "inputs": ["Banking financing Asset Manager Gets OK To Appeal €15M Fee Payout Ruling",
+                   "NASA's New Planet-Hunting Telescope Just Found Its First Earth-Sized World"]}
+    output_json = experiment['predictor'].json_to_json(input_json=input_json)
+    logger.info(input_json)
+    logger.info(output_json)
