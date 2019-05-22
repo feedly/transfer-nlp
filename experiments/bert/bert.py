9f2b59bbbec28155ca5c9287e84fd1422e4ff938
Peter Martigny <peter.martigny@gmail.com>
2019-05-21 20:35:35 -0700

    reduce bert code len

--- experiments/bert/bert.py
+++ experiments/bert/bert.py
@@ -15,60 +15,33 @@ tqdm.pandas()
 
 @register_plugin
 class BertVectorizer(Vectorizer):
-    def __init__(self, data_file: str):
+    def __init__(self, data_file: str, bert_version: str):
         super().__init__(data_file=data_file)
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
+        self.tokenizer = BertTokenizer.from_pretrained(bert_version)
         df = pd.read_csv(data_file)
-        target_vocab = Vocabulary(add_unk=False)
-        for category in sorted(set(df.category)):
-            target_vocab.add_token(category)
-        self.target_vocab = target_vocab
+        self.target_vocab = Vocabulary(add_unk=False)
+        self.target_vocab.add_many(set(df.category))
 
     def vectorize(self, title: str, max_seq_length: int) -> Tuple[np.array, np.array, np.array]:
-        tokens = self.tokenizer.tokenize(title)
-        tokens = ["[CLS]"] + tokens + ["[SEP]"]
-        token_type_ids = [0] * len(tokens)
-        input_ids = self.tokenizer.convert_tokens_to_ids(tokens)
-        attention_mask = [1] * len(input_ids)
-        padding = [0] * (max_seq_length - len(input_ids))
-        input_ids += padding
-        attention_mask += padding
-        token_type_ids += padding
-
+        tokens = ["[CLS]"] + self.tokenizer.tokenize(title) + ["[SEP]"]
+        token_type_ids, input_ids = [0] * len(tokens), self.tokenizer.convert_tokens_to_ids(tokens)
+        attention_mask, padding = [1] * len(input_ids), [0] * (max_seq_length - len(input_ids))
+        input_ids, attention_mask, token_type_ids = [x + padding for x in [input_ids, attention_mask, token_type_ids]]
         return np.array(input_ids), np.array(attention_mask), np.array(token_type_ids)
 
 
 @register_plugin
-class BertDataset(DatasetSplits):
+class BertDataloader(DatasetSplits):
 
-    def __init__(self, data_file: str, batch_size: int, vectorizer: Vectorizer):
-        self.df = pd.read_csv(data_file)
-        np.random.shuffle(self.df.values)  # Use this code in dev mode
-        N = 1000
-        self.df = self.df.head(n=N)
-
-        # preprocessing
+    def __init__(self, data_file: str, batch_size: int, max_sequence: int, vectorizer: Vectorizer):
         self.vectorizer: Vectorizer = vectorizer
-
-        self.max_sequence = 0
-        for title in tqdm(self.df.title, desc="Getting max sequence"):
-            tokens = self.vectorizer.tokenizer.tokenize(text=title)
-            self.max_sequence = max(self.max_sequence, len(tokens))
-        self.max_sequence += 2
-
-        vectors = self.df['title'].progress_apply(lambda x: self.vectorizer.vectorize(title=x, max_seq_length=self.max_sequence))
-        self.df['input_ids'] = vectors.progress_apply(lambda x: x[0])
-        self.df['attention_mask'] = vectors.progress_apply(lambda x: x[1])
-        self.df['token_type_ids'] = vectors.progress_apply(lambda x: x[2])
-        self.df['y_target'] = self.df['category'].progress_apply(lambda x: self.vectorizer.target_vocab.lookup_token(x))
-
-        train_df = self.df[self.df.split == 'train'][['input_ids', 'attention_mask', 'token_type_ids', 'y_target']]
-        val_df = self.df[self.df.split == 'val'][['input_ids', 'attention_mask', 'token_type_ids', 'y_target']]
-        test_df = self.df[self.df.split == 'test'][['input_ids', 'attention_mask', 'token_type_ids', 'y_target']]
-
-        super().__init__(train_set=DataFrameDataset(train_df), train_batch_size=batch_size,
-                         val_set=DataFrameDataset(val_df), val_batch_size=batch_size,
+        self.max_sequence: int = max_sequence + 2
+        df = pd.read_csv(data_file)
+        df[['input_ids', 'attention_mask', 'token_type_ids']] = df.progress_apply(
+            lambda row: pd.Series(self.vectorizer.vectorize(title=row['title'], max_seq_length=self.max_sequence)), axis=1)
+        df['y_target'] = df['category'].progress_apply(lambda x: self.vectorizer.target_vocab.lookup_token(x))
+        train_df, val_df, test_df = (df[df.split == mode][['input_ids', 'attention_mask', 'token_type_ids', 'y_target']] for mode in ['train', 'val', 'test'])
+        super().__init__(train_set=DataFrameDataset(train_df), train_batch_size=batch_size, val_set=DataFrameDataset(val_df), val_batch_size=batch_size,
                          test_set=DataFrameDataset(test_df), test_batch_size=batch_size)
 
 
