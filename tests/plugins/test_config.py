f45b483863e0383ecce34211fb5e2eac876b8251
Peter Martigny <peter.martigny@gmail.com>
2019-07-30 11:23:43 -0700

    wip

--- tests/plugins/test_config.py
+++ tests/plugins/test_config.py
@@ -140,7 +140,6 @@ register_plugin(DemoClassMethod.from_example, alias='from_example_alias_name')
 
 
 class RegistryTest(unittest.TestCase):
-     
     def test_class_method(self):
         
         experiment = {
